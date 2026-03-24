860d1aa8cd0f47c1f9461a22b3d35d7e3d1a3cae
reuben2005RT <rthomas4@bellarmine.edu>
2026-03-24 14:55:44 -0400

    Add files via upload

---
+++ creditcard/Ar_UListCC_Driver.java
@@ -0,0 +1,21 @@
+package creditcard;
+
+public class Ar_UListCC_Driver {
+public static void main(String args[]) throws Exception{
+Ar_UListCC testList = new AR_UListCC();
+scanner rewScanner = new scanner(new File("src/creditcard_balanced.csv"));
+sanner colScanner;
+rowScanner.nextLine(){
+	while )rowScanner.hasNextLine()){
+		string rowString = rowScanner.nextLine(){
+			colScanner = new scanner(rowScanner);
+			colScanner.useDelimiter(",");
+			int newtime = Integer.parseInt(colScanner.next());
+			double newamount = double.parseint(colScanner.next());
+			int newClass = Integer.parseInt(colScanner.next());
+			CCFraudrecord newREc = new CCFraudRecord(Newtime, newamount)
+					testList.putitem(newRecc);
+			}
+	}
+}
+}
